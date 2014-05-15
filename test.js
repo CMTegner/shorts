6d29e8375a05258bc8c6b751896ea2b5ac9195dd
Christian Tegnér <christian.tegner@gmail.com>
2014-05-15 19:45:47 +0200

    Add another test; correct existing test

--- test.js
+++ test.js
@@ -8,10 +8,19 @@ test('setup', function(t) {
 });
 
 test('POST', function(t) {
-    request.post('http://localhost:15555', { uri: 'http://npmjs.org' }, function(err, response) {
+    request.post('http://localhost:15555', { uri: 'https://npmjs.org' }, function(err, response) {
         t.equal(err, null, 'should not err');
         t.equal(response.statusCode, 302, 'should result in a \'302: found\'');
-        t.equal(response.headers.location, 'should return the shortened url in the \'location\' header');
+        t.equal(response.headers.location, 'http://localhost:15555/a', 'should return the shortened url in the \'location\' header');
+        t.end();
+    });
+});
+
+test('GET', function(t) {
+    request('http://localhost:15555/a' function(err, response) {
+        t.equal(err, null, 'should not err');
+        t.equal(response.statusCode, 301, 'should result in a \'301: moved permanently\'');
+        t.equal(response.headers.location, 'https://npmjs.org', 'should return the original url in the \'location\' header');
         t.end();
     });
 });
